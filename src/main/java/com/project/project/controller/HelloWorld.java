d19ed9e4373161ae6ede8da522812a3155ae8ea5
subrojitghosh123@gmail.com <subrojitghosh123@gmail.com>
2025-04-04 12:13:29 +0530

    demo-app

---
+++ src/main/java/com/project/project/controller/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.project.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return "Hello World from psa";
+    }
+}
